f56283ed6df57c261ad29886ed76f349c96dbaa6
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-06-25 16:08:46 +0200

    Update app.js

--- Unit_7 React/1 - First Steps in React/2 - Add React to a Project/scoreboard/app.js
+++ Unit_7 React/1 - First Steps in React/2 - Add React to a Project/scoreboard/app.js
@@ -3,29 +3,39 @@
 2 [props]object or null, 
 3 contnets or children of the element or null
 **/
-//option 1 of react syntaxs
-const title = <h1>My first react element!</h1>
 
-//option 2 of react syntaxs
-const desc = React.createElement(
-    'p',
-    null,
-    'MY gosh this is so cool I learned how to add stuff to the dom with react!!!!!'
-);
+//const header = React.createElement(
+//    'header',
+//    { id: 'headerOne' },
+//    title,
+//    desc
+//);
 
-const header = React.createElement(
-    'header',
-    { id: 'headerOne' },
-    title,
-    desc
-);
-/* 
-Render takes 2 properties
-1. the object you want to render.
-2. the location on the DOM that you want to render to.
-**/
-ReactDOM.render(
-    header, 
-    document.getElementById('root')
-);
+// const header = (
+//     <header>
+//       <h1>My first react element!</h1>
+//       <p>MY gosh this is so cool I learned how to add stuff to the dom with react!!!!!</p>;
+//     </header>
+//     );  
 
+const title = 'My first react element!';
+const titleId = 'main-title';
+const desc = "MY gosh this is so cool I learned how to add stuff to the dom with react!!!!!";
+const header = (
+    <header>
+      <h1 id={ titleId }> { title } </h1>
+      <p className="main-desc"> { desc }</p>;
+    </header>
+    );  
+      
+    /* 
+    Render takes 2 properties
+    1. the object you want to render.
+    2. the location on the DOM that you want to render to.
+    **/
+    ReactDOM.render(
+        header, 
+        document.getElementById('root')
+    );
+    
+    
